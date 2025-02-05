3c15a069fdd67d558ef21554fe5b5e21d28ff1e5
bholmes-learning <bh.learning.studies@gmail.com>
2025-02-05 18:37:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,116 +1,97 @@
 resource "azurerm_resource_group" "bhmcitrg01" {
   name     = "mcit_resource_group_bh01"
-  location = var.resource_group_location
+  location = "canadacentral"
 }
 
-resource "azurerm_virtual_network" "azfw_vnet" {
-  name                = "azfw-vnet"
-  location            = azurerm_resource_group.bhmcitrg01.location
-  resource_group_name = azurerm_resource_group.bhmcitrg01.name
-  address_space       = ["10.10.0.0/24"]
-}
+resource "azurerm_web_application_firewall_policy" "braeden_mcit_waf" {
+  name                = "braedenmcitwaf"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
 
-resource "azurerm_ip_group" "workload_ip_group" {
-  name                = "workload-ip-group"
-  resource_group_name = azurerm_resource_group.bhmcitrg01.name
-  location            = azurerm_resource_group.bhmcitrg01.location
-  cidrs               = ["10.20.0.0/24", "10.30.0.0/24"]
-}
-resource "azurerm_ip_group" "infra_ip_group" {
-  name                = "infra-ip-group"
-  resource_group_name = azurerm_resource_group.bhmcitrg01.name
-  location            = azurerm_resource_group.bhmcitrg01.location
-  cidrs               = ["10.40.0.0/24", "10.50.0.0/24"]
-}
+  custom_rules {
+    name      = "Rule1"
+    priority  = 1
+    rule_type = "MatchRule"
 
-resource "azurerm_subnet" "azfw_subnet" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.bhmcitrg01.name
-  virtual_network_name = azurerm_virtual_network.azfw_vnet.name
-  address_prefixes     = ["10.10.0.0/26"]
-}
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
 
-resource "azurerm_public_ip" "pip_azfw" {
-  name                = "pip-azfw"
-  location            = azurerm_resource_group.bhmcitrg01.location
-  resource_group_name = azurerm_resource_group.bhmcitrg01.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24", "10.0.0.0/24"]
+    }
 
-resource "azurerm_firewall_policy" "azfw_policy" {
-  name                     = "azfw-policy"
-  resource_group_name      = azurerm_resource_group.bhmcitrg01.name
-  location                 = azurerm_resource_group.bhmcitrg01.location
-  sku                      = var.firewall_sku_tier
-  threat_intelligence_mode = "Alert"
-}
+    action = "Block"
+  }
 
-resource "azurerm_firewall_policy_rule_collection_group" "net_policy_rule_collection_group" {
-  name               = "DefaultNetworkRuleCollectionGroup"
-  firewall_policy_id = azurerm_firewall_policy.azfw_policy.id
-  priority           = 200
-  network_rule_collection {
-    name     = "DefaultNetworkRuleCollection"
-    action   = "Allow"
-    priority = 200
-    rule {
-      name                  = "time-windows"
-      protocols             = ["UDP"]
-      source_ip_groups      = [azurerm_ip_group.workload_ip_group.id, azurerm_ip_group.infra_ip_group.id]
-      destination_ports     = ["123"]
-      destination_addresses = ["132.86.101.172"]
+  custom_rules {
+    name      = "Rule2"
+    priority  = 2
+    rule_type = "MatchRule"
+
+    match_conditions {
+      match_variables {
+        variable_name = "RemoteAddr"
+      }
+
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["192.168.1.0/24"]
+    }
+
+    match_conditions {
+      match_variables {
+        variable_name = "RequestHeaders"
+        selector      = "UserAgent"
+      }
+
+      operator           = "Contains"
+      negation_condition = false
+      match_values       = ["Windows"]
+    }
+
+    action = "Block"
+  }
+
+  policy_settings {
+    enabled                     = true
+    mode                        = "Prevention"
+    request_body_check          = true
+    file_upload_limit_in_mb     = 100
+    max_request_body_size_in_kb = 128
+  }
+
+  managed_rules {
+    exclusion {
+      match_variable          = "RequestHeaderNames"
+      selector                = "x-company-secret-header"
+      selector_match_operator = "Equals"
+    }
+    exclusion {
+      match_variable          = "RequestCookieNames"
+      selector                = "too-tasty"
+      selector_match_operator = "EndsWith"
+    }
+
+    managed_rule_set {
+      type    = "OWASP"
+      version = "3.2"
+      rule_group_override {
+        rule_group_name = "REQUEST-920-PROTOCOL-ENFORCEMENT"
+        rule {
+          id      = "920300"
+          enabled = true
+          action  = "Log"
+        }
+
+        rule {
+          id      = "920440"
+          enabled = true
+          action  = "Block"
+        }
+      }
     }
   }
-}
-
-resource "azurerm_firewall_policy_rule_collection_group" "app_policy_rule_collection_group" {
-  name               = "DefaulApplicationtRuleCollectionGroup"
-  firewall_policy_id = azurerm_firewall_policy.azfw_policy.id
-  priority           = 300
-  application_rule_collection {
-    name     = "DefaultApplicationRuleCollection"
-    action   = "Allow"
-    priority = 500
-    rule {
-      name = "AllowWindowsUpdate"
-
-      description = "Allow Windows Update"
-      protocols {
-        type = "Http"
-        port = 80
-      }
-      protocols {
-        type = "Https"
-        port = 443
-      }
-      source_ip_groups      = [azurerm_ip_group.workload_ip_group.id, azurerm_ip_group.infra_ip_group.id]
-      destination_fqdn_tags = ["WindowsUpdate"]
-    }
-    rule {
-      name        = "Global Rule"
-      description = "Allow access to Microsoft.com"
-      protocols {
-        type = "Https"
-        port = 443
-      }
-      destination_fqdns = ["*.microsoft.com"]
-      terminate_tls     = false
-      source_ip_groups  = [azurerm_ip_group.workload_ip_group.id, azurerm_ip_group.infra_ip_group.id]
-    }
-  }
-}
-
-resource "azurerm_firewall" "fw" {
-  name                = "azfw"
-  location            = azurerm_resource_group.bhmcitrg01.location
-  resource_group_name = azurerm_resource_group.bhmcitrg01.name
-  sku_name            = "AZFW_VNet"
-  sku_tier            = var.firewall_sku_tier
-  ip_configuration {
-    name                 = "azfw-ipconfig"
-    subnet_id            = azurerm_subnet.azfw_subnet.id
-    public_ip_address_id = azurerm_public_ip.pip_azfw.id
-  }
-  firewall_policy_id = azurerm_firewall_policy.azfw_policy.id
 }
